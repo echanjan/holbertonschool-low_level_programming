10ac956343abe8bb3b15516589c8f2b4b2d3dc15
echanjan <erickchanjan300696@.com>
2023-08-03 22:57:06 -0700

    update 5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - imprime usando funcion printf
- * no esta permitido usar funcion puts
- * return (0)
- * el programa debe compilarse sin previo aviso al usar la opción -Wall gcc
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
+
