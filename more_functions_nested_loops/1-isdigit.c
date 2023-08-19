705020f4949b226748df2f2d9548ee9c154640b9
echanjan <erickchanjan300696@.com>
2023-08-19 05:46:38 -0700

    update 1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _isdigit - check the given value is a digit
+* @c: values to check
+*
+* Return: 1 if digit else 0 (False)
+*
+*/
+int _isdigit(int c)
+{
+	if (c >= 0 && c < 10)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
