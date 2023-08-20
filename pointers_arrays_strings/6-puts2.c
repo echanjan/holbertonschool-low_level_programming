aee4c0dfcedcce33abd2e425f6b19f6dd8775d76
echanjan <erickchanjan300696@.com>
2023-08-20 16:21:13 -0700

    update 6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - Printin stdout for two character. starting the first character.
+ * @str: String a print.
+ *
+ * Return: void.
+ */
+void puts2(char *str)
+{
+	int i;
+	int o = 1;
+	const int NEW_LINE = 10;
+
+	i = strlen(str);
+	if (i != 0)
+	{
+		i--;
+		putchar(str[0]);
+
+		while (o <= i)
+		{
+			if (o % 2 == 0)
+				putchar(str[o]);
+			o++;
+		}
+	}
+	putchar(NEW_LINE);
+}
