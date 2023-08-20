f264aaaf661db7f0004bd59e33f6fa7c2e0101a3
echanjan <erickchanjan300696@.com>
2023-08-20 12:45:28 -0700

    update 0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - update value of parameter,using pointer.
+ * @n: pointer to update.
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
