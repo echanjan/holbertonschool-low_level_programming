de7cb416d32cb30a24d46c8b063e1b0dd260385d
echanjan <erickchanjan300696@.com>
2023-08-27 17:07:11 -0700

    update 2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,13 +1,18 @@
-#include <stdio>
+#include <stdio.h>
 /**
- *
- *
- *
- */
-void main (int argc, char *argv [])
+* main - Entry point, This program print the args in stdout.
+* @argc: Number of args.
+* @argv: Args
+*
+* Return: Success 0.
+*/
+int main(int argc, char const *argv[])
 {
-	(void)argv;
+	int i;
 
-	printf("%d\n", argc - 1);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
