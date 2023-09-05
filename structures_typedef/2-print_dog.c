be0b50f1ce4a4d45640c6195288da4fc0dc5adcf
echanjan <erickchanjan300696@.com>
2023-09-04 20:59:25 -0700

    update 2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - This function print a struct.
+ * @d: Received a struct initialized.
+ *
+ * Return: void.
+ */
+void print_dog(struct dog *d)
+{
+	const char *NIL = "nil";
+
+	if (d == NULL)
+		return;
+	if ((*d).name == NULL)
+		printf("Name: (%s)\n", NIL);
+	else
+		printf("Name: %s\n", (*d).name);
+
+	printf("Age: %f\n", (*d).age);
+
+	if ((*d).owner == NULL)
+		printf("Owner: (%s)\n", NIL);
+	else
+		printf("Owner: %s\n", (*d).owner);
+}
